a4fbb18e0b1674572bae4d32445da3bfaa5b1aac
Waqas Rasheed <waqasr.consultant@herbalife.com>
2016-09-14 12:50:19 +0800

    initial check in.

---
+++ GaugeDbWrapper/GaugeDbWrapper.Droid/obj/Debug/android/src/md5832872fe3fddc3d28a4b60b9b91d6839/MainActivity.java
@@ -0,0 +1,47 @@
+package md5832872fe3fddc3d28a4b60b9b91d6839;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("GaugeDbWrapper.Droid.MainActivity, GaugeDbWrapper.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("GaugeDbWrapper.Droid.MainActivity, GaugeDbWrapper.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
